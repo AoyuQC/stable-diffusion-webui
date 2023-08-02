1448659da09750b4af16a4eec6bab45150ff7437
AoyuQC <aoyuzhang1989@gmail.com>
2023-08-02 09:27:48 +0000

    feat: add pipeline

--- modules/api/api.py
+++ modules/api/api.py
@@ -33,6 +33,9 @@ import piexif
 import piexif.helper
 from contextlib import closing
 
+# TODO move to build_scripts
+from modules.pipeline import process_images_pipeline
+
 
 def script_name_to_index(name, scripts):
     try:
@@ -208,6 +211,9 @@ class Api:
         self.add_api_route("/sdapi/v1/scripts", self.get_scripts_list, methods=["GET"], response_model=models.ScriptsList)
         self.add_api_route("/sdapi/v1/script-info", self.get_script_info, methods=["GET"], response_model=List[models.ScriptInfo])
 
+        # TODO move to build_scripts
+        self.add_api_route("/invocations", self.invocations, methods=["POST"], response_model=[])
+
         if shared.cmd_opts.api_server_stop:
             self.add_api_route("/sdapi/v1/server-kill", self.kill_webui, methods=["POST"])
             self.add_api_route("/sdapi/v1/server-restart", self.restart_webui, methods=["POST"])
@@ -333,16 +339,14 @@ class Api:
                 p.outpath_grids = opts.outdir_txt2img_grids
                 p.outpath_samples = opts.outdir_txt2img_samples
 
-                try:
-                    shared.state.begin(job="scripts_txt2img")
-                    if selectable_scripts is not None:
-                        p.script_args = script_args
-                        processed = scripts.scripts_txt2img.run(p, *p.script_args) # Need to pass args as list here
-                    else:
-                        p.script_args = tuple(script_args) # Need to pass args as tuple here
-                        processed = process_images(p)
-                finally:
-                    shared.state.end()
+                shared.state.begin(job="scripts_txt2img")
+                if selectable_scripts is not None:
+                    p.script_args = script_args
+                    processed = scripts.scripts_txt2img.run(p, *p.script_args) # Need to pass args as list here
+                else:
+                    p.script_args = tuple(script_args) # Need to pass args as tuple here
+                    processed = process_images(p)
+                shared.state.end()
 
         b64images = list(map(encode_pil_to_base64, processed.images)) if send_images else []
 
@@ -392,16 +396,14 @@ class Api:
                 p.outpath_grids = opts.outdir_img2img_grids
                 p.outpath_samples = opts.outdir_img2img_samples
 
-                try:
-                    shared.state.begin(job="scripts_img2img")
-                    if selectable_scripts is not None:
-                        p.script_args = script_args
-                        processed = scripts.scripts_img2img.run(p, *p.script_args) # Need to pass args as list here
-                    else:
-                        p.script_args = tuple(script_args) # Need to pass args as tuple here
-                        processed = process_images(p)
-                finally:
-                    shared.state.end()
+                shared.state.begin(job="scripts_img2img")
+                if selectable_scripts is not None:
+                    p.script_args = script_args
+                    processed = scripts.scripts_img2img.run(p, *p.script_args) # Need to pass args as list here
+                else:
+                    p.script_args = tuple(script_args) # Need to pass args as tuple here
+                    processed = process_images(p)
+                shared.state.end()
 
         b64images = list(map(encode_pil_to_base64, processed.images)) if send_images else []
 
@@ -724,9 +726,9 @@ class Api:
             cuda = {'error': f'{err}'}
         return models.MemoryResponse(ram=ram, cuda=cuda)
 
-    def launch(self, server_name, port, root_path):
+    def launch(self, server_name, port):
         self.app.include_router(self.router)
-        uvicorn.run(self.app, host=server_name, port=port, timeout_keep_alive=shared.cmd_opts.timeout_keep_alive, root_path=root_path)
+        uvicorn.run(self.app, host=server_name, port=port, timeout_keep_alive=shared.cmd_opts.timeout_keep_alive)
 
     def kill_webui(self):
         restart.stop_program()
@@ -740,3 +742,121 @@ class Api:
         shared.state.server_command = "stop"
         return Response("Stopping.")
 
+    # TODO move to build_scripts
+    def invocations(self, req: InvocationsRequest):
+        """
+        @return:
+        """
+        logger.info('-------invocation------')
+        logger.info("Loading Sagemaker API Endpoints.")
+
+        def show_slim_dict(payload):
+            pay_type = type(payload)
+            if pay_type is dict:
+                for k, v in payload.items():
+                    logger.info(f"{k}")
+                    show_slim_dict(v)
+            elif pay_type is list:
+                for v in payload:
+                    logger.info(f"list")
+                    show_slim_dict(v)
+            elif pay_type is str:
+                if len(payload) > 50:
+                    logger.info(f" : {len(payload)} contents")
+                else:
+                    logger.info(f" : {payload}")
+            else:
+                logger.info(f" : {payload}")
+
+        logger.info(f"{threading.current_thread().ident}_{threading.current_thread().name}")
+        logger.info(f"task is {req.task}")
+        logger.info(f"checkpoint_info is {req.checkpoint_info}")
+        logger.info(f"models is {req.models}")
+        logger.info(f"txt2img_payload is: ")
+        txt2img_payload = {} if req.txt2img_payload is None else json.loads(req.txt2img_payload.json())
+        show_slim_dict(txt2img_payload)
+        logger.info(f"img2img_payload is: ")
+        img2img_payload = {} if req.img2img_payload is None else json.loads(req.img2img_payload.json())
+        show_slim_dict(img2img_payload)
+        logger.info(f"extra_single_payload is: ")
+        extra_single_payload = {} if req.extras_single_payload is None else json.loads(
+            req.extras_single_payload.json())
+        show_slim_dict(extra_single_payload)
+        logger.info(f"extra_batch_payload is: ")
+        extra_batch_payload = {} if req.extras_batch_payload is None else json.loads(
+            req.extras_batch_payload.json())
+        show_slim_dict(extra_batch_payload)
+        logger.info(f"interrogate_payload is: ")
+        interrogate_payload = {} if req.interrogate_payload is None else json.loads(
+            req.interrogate_payload.json())
+        show_slim_dict(interrogate_payload)
+        # logger.info(f"db_create_model_payload is: ")
+        # logger.info(f"{req.db_create_model_payload}")
+        # logger.info(f"merge_checkpoint_payload is: ")
+        # logger.info(f"{req.merge_checkpoint_payload}")
+        # logger.info(f"json is {json.loads(req.json())}")
+        try:
+            if req.task == 'txt2img':
+                with self.queue_lock:
+                    response = self.txt2img_pipeline(req.txt2img_payload)
+                    logger.info(
+                        f"{threading.current_thread().ident}_{threading.current_thread().name}_______ txt2img end !!!!!!!! {len(response.json())}")
+                    return response.json()
+            elif req.task == 'img2img':
+                logger.info("img2img not implemented!")
+                return 0
+            elif req.task == 'interrogate_clip' or req.task == 'interrogate_deepbooru':
+                logger.info("interrogate not implemented!")
+                return 0
+            elif req.task == 'db-create-model':
+                logger.info("db-create-model not implemented!")
+                return 0
+            else:
+                raise NotImplementedError
+        except Exception as e:
+            traceback.print_exc()
+
+    def txt2img_pipeline(self, txt2imgreq: models.StableDiffusionTxt2ImgProcessingAPI):
+        script_runner = scripts.scripts_txt2img
+        if not script_runner.scripts:
+            script_runner.initialize_scripts(False)
+            ui.create_ui()
+        if not self.default_script_arg_txt2img:
+            self.default_script_arg_txt2img = self.init_default_script_args(script_runner)
+        selectable_scripts, selectable_script_idx = self.get_selectable_script(txt2imgreq.script_name, script_runner)
+
+        populate = txt2imgreq.copy(update={  # Override __init__ params
+            "sampler_name": validate_sampler_name(txt2imgreq.sampler_name or txt2imgreq.sampler_index),
+            "do_not_save_samples": not txt2imgreq.save_images,
+            "do_not_save_grid": not txt2imgreq.save_images,
+        })
+        if populate.sampler_name:
+            populate.sampler_index = None  # prevent a warning later on
+
+        args = vars(populate)
+        args.pop('script_name', None)
+        args.pop('script_args', None) # will refeed them to the pipeline directly after initializing them
+        args.pop('alwayson_scripts', None)
+
+        script_args = self.init_script_args(txt2imgreq, self.default_script_arg_txt2img, selectable_scripts, selectable_script_idx, script_runner)
+
+        send_images = args.pop('send_images', True)
+        args.pop('save_images', None)
+
+        with closing(StableDiffusionProcessingTxt2Img(sd_model=None, **args)) as p:
+            p.scripts = script_runner
+            p.outpath_grids = opts.outdir_txt2img_grids
+            p.outpath_samples = opts.outdir_txt2img_samples
+
+            # shared.state.begin(job="scripts_txt2img")
+            if selectable_scripts is not None:
+                p.script_args = script_args
+                processed = scripts.scripts_txt2img.run(p, *p.script_args) # Need to pass args as list here
+            else:
+                p.script_args = tuple(script_args) # Need to pass args as tuple here
+                processed = process_images(p)
+            # shared.state.end()
+
+        b64images = list(map(encode_pil_to_base64, processed.images)) if send_images else []
+
+        return models.TextToImageResponse(images=b64images, parameters=vars(txt2imgreq), info=processed.js())
