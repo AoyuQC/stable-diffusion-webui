617c712de7572474bea58bccba5a7f5b4418e8fd
yuxiao <xiaoyu_cindy@msn.com>
2023-08-08 09:43:43 +0000

    update pipeline

--- modules/api/api.py
+++ modules/api/api.py
@@ -864,7 +864,7 @@ class Api:
                 processed = scripts.scripts_txt2img.run(p, *p.script_args) # Need to pass args as list here
             else:
                 p.script_args = tuple(script_args) # Need to pass args as tuple here
-                processed = process_images_pipeline(p)
+                processed = process_images(p)
             # shared.state.end()
 
         b64images = list(map(encode_pil_to_base64, processed.images)) if send_images else []
