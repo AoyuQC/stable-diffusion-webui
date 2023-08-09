faf73ec42804a73dab0db0f6bac3cb3accff8630
yuxiao <xiaoyu_cindy@msn.com>
2023-08-09 05:57:01 +0000

    update diffusers pipeline

--- modules/pipeline.py
+++ modules/pipeline.py
@@ -26,6 +26,7 @@ import modules.sd_models as sd_models
 import modules.sd_vae as sd_vae
 from ldm.data.util import AddMiDaS
 from ldm.models.diffusion.ddpm import LatentDepth2ImageDiffusion
+from tqdm import tqdm
 
 from einops import repeat, rearrange
 from blendmodes.blend import blendLayers, BlendType
@@ -1429,6 +1430,7 @@ class StableDiffusionPipelineTxt2Img(StableDiffusionProcessing):
         self.unet = pipeline.unet
         self.scheduler = pipeline.scheduler
         self.text_encoder = pipeline.text_encoder
+        self.decode_latents = pipeline.decode_latents
         self.vae = pipeline.vae
         if self.enable_hr:
             if self.hr_sampler_name is not None and self.hr_sampler_name != self.sampler_name:
@@ -1509,99 +1511,16 @@ class StableDiffusionPipelineTxt2Img(StableDiffusionProcessing):
                 raise Exception(f"could not find upscaler named {self.hr_upscaler}")
 
         x = create_random_tensors([opt_C, self.height // opt_f, self.width // opt_f], seeds=seeds, subseeds=subseeds, subseed_strength=self.subseed_strength, seed_resize_from_h=self.seed_resize_from_h, seed_resize_from_w=self.seed_resize_from_w, p=self)
-        # samples = self.sampler.sample(self, x, conditioning, unconditional_conditioning, image_conditioning=self.txt2img_image_conditioning(x))
 
-        generator = torch.Generator(device=torch_device)
-        thisSeed = 12345
-        generator = generator.manual_seed(thisSeed)
-        
-        from typing import List, Optional, Tuple, Union
-        def randn_tensor(
-            shape: Union[Tuple, List],
-            generator: Optional[Union[List["torch.Generator"], "torch.Generator"]] = None,
-            device: Optional["torch.device"] = None,
-            dtype: Optional["torch.dtype"] = None,
-            layout: Optional["torch.layout"] = None,
-        ):
-            """A helper function to create random tensors on the desired `device` with the desired `dtype`. When
-            passing a list of generators, you can seed each batch size individually. If CPU generators are passed, the tensor
-            is always created on the CPU.
-            """
-            # device on which tensor is created defaults to device
-            rand_device = device
-            batch_size = shape[0]
-
-            layout = layout or torch.strided
-            device = device or torch.device("cpu")
-
-            if generator is not None:
-                gen_device_type = generator.device.type if not isinstance(generator, list) else generator[0].device.type
-                if gen_device_type != device.type and gen_device_type == "cpu":
-                    rand_device = "cpu"
-                    if device != "mps":
-                        print(
-                            f"The passed generator was created on 'cpu' even though a tensor on {device} was expected."
-                            f" Tensors will be created on 'cpu' and then moved to {device}. Note that one can probably"
-                            f" slighly speed up this function by passing a generator that was created on the {device} device."
-                        )
-                elif gen_device_type != device.type and gen_device_type == "cuda":
-                    raise ValueError(f"Cannot generate a {device} tensor from a generator of type {gen_device_type}.")
-
-            # make sure generator list of length 1 is treated like a non-list
-            if isinstance(generator, list) and len(generator) == 1:
-                generator = generator[0]
-
-            if isinstance(generator, list):
-                shape = (1,) + shape[1:]
-                latents = [
-                    torch.randn(shape, generator=generator[i], device=rand_device, dtype=dtype, layout=layout)
-                    for i in range(batch_size)
-                ]
-                latents = torch.cat(latents, dim=0).to(device)
-            else:
-                latents = torch.randn(shape, generator=generator, device=rand_device, dtype=dtype, layout=layout).to(device)
-
-            return latents
-
-
-        def prepare_latents(self, batch_size, num_channels_latents, height, width, dtype, device, generator, latents=None):
-            shape = (batch_size, num_channels_latents, height // self.vae_scale_factor, width // self.vae_scale_factor)
-            if isinstance(generator, list) and len(generator) != batch_size:
-                raise ValueError(
-                    f"You have passed a list of generators of length {len(generator)}, but requested an effective batch"
-                    f" size of {batch_size}. Make sure the batch size matches the length of the generators."
-                )
-
-            if latents is None:
-                latents = randn_tensor(shape, generator=generator, device=device, dtype=dtype)
-            else:
-                latents = latents.to(device)
-
-            # scale the initial noise by the standard deviation required by the scheduler
-            latents = latents * self.scheduler.init_noise_sigma
-            return latents
-
-        # diffuser pipeline
-        latents = x
-
-        from tqdm.auto import tqdm
-
-        prompt = "a photograph of an astronaut riding a horse"
-        height = 512
-        width = 512
-        num_inference_steps = 25
-        guidance_scale = 7.5
-        torch_device = "cuda"
-        image_middle = pipeline(prompt).images[0]
-
-        # text embedding
+        # prompt text embedding
         text_input = self.tokenizer(
-            prompt, padding="max_length", max_length=self.tokenizer.model_max_length, truncation=True, return_tensors="pt"
+            self.prompt, padding="max_length", max_length=self.tokenizer.model_max_length, truncation=True, return_tensors="pt"
         )
         with torch.no_grad():
-            text_embeddings = self.text_encoder(text_input.input_ids.to(torch_device))[0]
+            text_embeddings = self.text_encoder(text_input.input_ids.to(shared.device))[0]
         
-        self.scheduler.set_timesteps(num_inference_steps)
+        # prepare timesteps
+        self.scheduler.set_timesteps(self.steps)
 
         for t in tqdm(self.scheduler.timesteps):
             # expand the latents if we are doing classifier-free guidance to avoid doing two forward passes.
@@ -1612,26 +1531,20 @@ class StableDiffusionPipelineTxt2Img(StableDiffusionProcessing):
 
             # predict the noise residual
             with torch.no_grad():
-                noise_pred = self.unet(latent_model_input, t, encoder_hidden_states=text_embeddings).sample
+                noise_pred = self.unet(latent_model_input, t, encoder_hidden_states=text_embeddings)
+                noise_pred = noise_pred.sample
 
             # perform guidance
             # noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
             # noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
 
             # compute the previous noisy sample x_t -> x_t-1
-            latents = self.scheduler.step(noise_pred, t, latents).prev_sample
+            latents = self.scheduler.step(noise_pred, t, latents)
+            latents = latents.prev_sample
 
         samples = latents
 
-        def decode_latents(latents):
-            latents = 1 / self.vae.config.scaling_factor * latents
-            image = self.vae.decode(latents).sample
-            image = (image / 2 + 0.5).clamp(0, 1)
-            # we always cast to float32 as this does not cause significant overhead and is compatible with bfloat16
-            image = image.cpu().permute(0, 2, 3, 1).float().numpy()
-            return image
-        
-        image = decode_latents(samples)
+        image = self.decode_latents(samples)
         # 9. Run safety checker
         # image, has_nsfw_concept = self.run_safety_checker(image, torch_device, prompt_embeds.dtype)
         # 10. Convert to PIL
